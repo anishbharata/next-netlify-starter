16afaf08d715f0db0402a124f67295b2d01065ea
anish.bharata <anish.bharata@gmail.com>
2022-06-17 21:39:40 -0400

    init

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="https://us.123rf.com/450wm/bpfoto/bpfoto1605/bpfoto160500050/58605128-simple-black-heart-isolated-over-a-white-background-.jpg?ver=6" alt="Enncode Logo" className={styles.logo} /> for you
       </footer>
     </>
   )
